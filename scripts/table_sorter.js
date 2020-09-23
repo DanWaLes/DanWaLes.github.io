f73a450795702d2f5c254b3e482c8679091731f0
DanWaLes <daniel.church@btinternet.com>
2020-09-23 08:32:35 +0100

    Update table_sorter.js

--- scripts/table_sorter.js
+++ scripts/table_sorter.js
@@ -139,8 +139,8 @@
 			const foundIndex = sortData.originals.indexOf(itemToFind);
 			const toMove = rows[foundIndex + offset];
 
-			toMove.dataset.pos = i;
-			toMove.parentNode.insertBefore(toMove, rows[i]);
+			toMove.setAttribute("data-pos", i);
+			// toMove.parentNode.insertBefore(toMove, rows[i]);
 
 			// rows[i + offset]rows[foundIndex].parentNode.insertBefore(rows[foundIndex], rows[i + offset].parentNode);
 			// can now delete item at found index, free up memory
