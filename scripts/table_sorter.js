1d39b7f3ed0e8599d9c60865092a597a6010bcd7
DanWaLes <daniel.church@btinternet.com>
2020-09-29 03:32:30 +0100

    Update table_sorter.js
    test

--- scripts/table_sorter.js
+++ scripts/table_sorter.js
@@ -151,7 +151,7 @@
 			const toFind = sortData[i];
 			const originalIndex = sortData.originals.indexOf(toFind);
 			const rowToMove = rows[originalIndex];
-			const rowToMoveBefore = rows[i/* - 1*/];
+			const rowToMoveBefore = rows[i - 1];
 
 			rowToMove.parentNode.insertBefore(rowToMove, rowToMoveBefore);
 			// prevent wrong row reference due to dope data
