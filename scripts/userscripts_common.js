13e4a3fcacf82bb46bfc77ae57b4eafe760dd8c4
dac90 <dac90.aber.ac.uk>
2021-02-03 02:05:08 +0000

    Update userscripts_common.js
    error not beign detected

--- scripts/userscripts_common.js
+++ scripts/userscripts_common.js
@@ -765,6 +765,7 @@
 		}
 
 		async function getTotalClanMembers() {
+			console.log(clanWindow.location.href);
 			if (clanWindow.location.href.match(/https:\/\/www\.warzone.com\/Error\?e=InvalidID/i)) {
 				console.log('error');
 				throw new Error('This clan does not exist');
