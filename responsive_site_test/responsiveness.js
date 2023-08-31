07cc40736757e96ff055df44254714f79fd82f83
DanWaLes <daniel.church@btinternet.com>
2023-09-01 00:01:38 +0100

    Add files via upload

--- responsive_site_test/responsiveness.js
+++ responsive_site_test/responsiveness.js
@@ -2,14 +2,18 @@
 	function resized() {
 		const body = document.getElementsByTagName('body')[0];
 		const bodyStyle = getComputedStyle(body);
-		const nav = document.getElementsByTagName('nav')[0];
-		const newHeight = window.innerHeight - Number(bodyStyle.marginTop.replace('px', '')) - Number(bodyStyle.marginBottom.replace('px', '')) + 'px';
 		const aside = document.getElementsByTagName('aside')[0];
-
-		nav.style.height = newHeight;
-		nav.style.width = aside.clientWidth + 'px';
-
+		const nav = document.getElementsByTagName('nav')[0];
 		const navButtons = document.querySelectorAll('nav ul li');
+		const swapNavBtn = document.getElementById('swapNavBtn');
+		const swapNavBtnStyle = getComputedStyle(swapNavBtn);
+		const content = document.getElementById('content');
+		const mvContent = document.getElementById('mvContent');
+		const header = document.getElementsByTagName('header')[0];
+		const footer = document.getElementsByTagName('footer')[0];
+
+		nav.style.height = window.innerHeight - (Number(bodyStyle.marginTop.replace('px', '')) * 2) - (Number(swapNavBtnStyle.paddingTop.replace('px', '')) * 4) + 'px';
+		nav.style.width = aside.clientWidth + 'px';
 
 		let biggestBtnSize = 0;
 
@@ -21,12 +25,12 @@
 			}
 		}
 
-		const content = document.getElementById('content');
-		const mvContent = document.getElementById('mvContent');
-		const header = document.getElementsByTagName('header')[0];
-		const footer = document.getElementsByTagName('footer')[0];
+		swapNavBtn.style.width = nav.clientWidth - Number(getComputedStyle(mvContent).marginLeft.replace('px', '')) + 'px';
+		mvContent.removeAttribute('style');
 
 		if (window.innerWidth < 600) {
+			mvContent.style.maxWidth = (window.innerWidth - (Number(bodyStyle.marginLeft.replace('px', '')) * 3) - nav.clientWidth) + 'px';
+
 			if (header.nextElementSibling == content) {
 				mvContent.insertAdjacentElement('afterbegin', header);
 			}
@@ -48,6 +52,39 @@
 		}
 	}
 
+	const lsKey_navDir = 'navDir';
+
+	function setupSwapNavBtn() {
+		if (!['false', 'true'].includes(localStorage[lsKey_navDir])) {
+			localStorage[lsKey_navDir] = 'false';
+		}
+
+		const swapNavBtn = document.getElementById('swapNavBtn');
+		swapNavBtn.onclick = function() {
+			localStorage[lsKey_navDir] = '' + !JSON.parse(localStorage[lsKey_navDir]);
+			moveNav();
+		};
+
+		moveNav();
+	}
+
+	function moveNav() {
+		const swapNavBtn = document.getElementById('swapNavBtn');
+		const aside = document.getElementsByTagName('aside')[0];
+		const mvContent = document.getElementById('mvContent');
+
+		swapNavBtn.innerText = JSON.parse(localStorage[lsKey_navDir]) ? '→' : '←';
+
+		if (!JSON.parse(localStorage[lsKey_navDir])) {
+			aside.className = 'right';
+			mvContent.className = 'left';
+		}
+		else {
+			aside.removeAttribute('class');
+			mvContent.removeAttribute('class'); 
+		}
+	}
+
 	function run() {
 		if (document.readyState != 'complete') {
 			return;
@@ -55,6 +92,7 @@
 
 		window.onresize = resized;
 		resized();
+		setupSwapNavBtn();
 	}
 
 	document.onreadystatechange = run;
